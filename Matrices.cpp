521ada3e7b9fd0ade0d1d360316c78b2093ba2bf
Eric Cartagena <cartagenaea@gmail.com>
2025-04-29 22:43:49 -0700

    Initial commit

---
+++ Matrices.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+// namepace Matrices
+// {
+//     Matrix::Matrix(int _rows, int_cols)
+//     {
+//         rows = _rows;
+//         cols = _cols;
+//     }
+// }
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;
+    return 0;
+}
+
+//  The actual source code below:
+
+// #include "Engine.h"
+// int main()
+// {
+// // Declare an instance of Engine
+// Engine engine;
+// // Start the engine
+// engine.run();
+// // Quit in the usual way when the engine is stopped
+// return 0;
+// }
