bf3eeef397f1e9bc55d03869a9de2b46d5660a04
Eric Cartagena <cartagenaea@gmail.com>
2025-05-06 18:55:24 -0700

    Particle.cpp completed: draw method completed; update method completed; appended and completed update, translate, and rotate methods

--- Particle.cpp
+++ Particle.cpp
@@ -37,12 +37,63 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 
 void Particle::draw(RenderTarget& target, RenderStates states) const
 {
-    
+    VertexArray lines(TriangleFan, m_numPoints + 1);
+    Vector2f center = (Vector2f)target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane);
+    lines[0].position = center;
+    lines[0].color = m_color1;
+    for(int j = 1; j <= m_numPoints; j++)
+    {
+        float x = m_A(0, j - 1);
+        float y = m_A(1, j - 1);
+
+        lines[j].position = (Vector2f)target.mapCoordsToPixel(Vector2f(x, y), m_cartesianPlane);
+        lines[j].color = m_color2;
+    }
+    target.draw(lines);
 }
 
 void Particle::update(float dt)
 {
+    m_ttl -= dt;
+    rotate(dt * m_radiansPerSec);
+    scale(SCALE);
 
+    float dx = m_vx * dt;
+    m_vy -= G * dt;
+    float dy = m_vy * dt;
+
+    translate(dx, dy);
+}
+
+void Particle::translate(double xShift, double yShift)
+{
+    TranslationMatrix T(xShift, yShift, 2);
+    m_A = T + m_A;
+    
+    m_centerCoordinate.x += xShift;
+    m_centerCoordinate.y += yShift;
+}
+
+void Particle::rotate(double theta)
+{
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+
+    RotationMatrix R  = theta;
+    m_A = R * m_A;
+    
+    translate(temp.x, temp.y);
+}
+
+void Particle::scale(double c)
+{
+    Vector2f temp = m_centerCoordinate;
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+
+    ScalingMatrix S(c);
+    m_A = S * m_A;
+
+    translate(temp.x, temp.y);
 }
 
 //Functions for unit testing
